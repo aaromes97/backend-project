7957e698aa38148db3f29dbd60de5b62b3f222d2
adridm97 <adri.d.m1997@gmail.com>
2022-02-21 10:56:07 +0100

    mongoose, models and index

---
+++ models/Anuncio.js
@@ -0,0 +1,17 @@
+"use strict";
+const mongoose = require("mongoose");
+
+//Definimos el esquema de Anuncio
+const anuncioSchema = mongoose.Schema({
+  nombre: String,
+  foto: String,
+  descripcion: String,
+  venta: Boolean,
+  precio: Number,
+  autor: String,
+  tags: [String],
+});
+
+const Anuncio = mongoose.model("Anuncio", anuncioSchema);
+
+module.exports = Anuncio;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const Anuncio = require("../../models/Anuncio");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", async (req, res, next) => {
+  const nombre = req.query.nombre;
+  const descripcion = req.query.descripcion;
+  const venta = req.query.venta;
+  const precio = req.query.precio;
+  const anuncios = await Anuncio.find({
+    nombre: nombre,
+    descripcion: descripcion,
+    venta: venta,
+    precio: precio,
+  });
+  res.render("index", { anuncios: anuncios });
 });
 
 module.exports = router;
