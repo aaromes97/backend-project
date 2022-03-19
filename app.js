9ca27fe72ee48611b5f780dfc83335c1440f5c7c
André Alcalde <andrealcalde2998@gmail.com>
2022-03-19 09:40:04 +0100

    socket.io implementend 2.0(socket not working)

--- app.js
+++ app.js
@@ -13,27 +13,15 @@ const forgotPassword = require("./routes/forgotPassword");
 var indexRouter = require("./routes/index");
 const bcrycpt = require("bcrypt");
 const Usuario = require("./models/Usuario");
-var STATIC_CHANNELS = [{
-  name: 'Global chat',
-  participants: 0,
-  id: 1,
-  sockets: []
-}, {
-  name: 'Funny',
-  participants: 0,
-  id: 2,
-  sockets: []
-}];
+const app = express();
+
+app.use(express());
 
 /* jshint ignore:start */
 require("./lib/connectMongoose");
 /* jshint ignore:end */
 // Cargamos las definiciones de todos nuestros modelos
 require("./models/Usuario");
-
-const app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -54,60 +42,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 const loginController = new LoginController();
-
-// CHAT LISTENER
-
-app.get('/api/chat', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'))
-});
-
-io.on('connection', (socket) => { // socket object may be used to send specific messages to the new connected client
-  console.log('new client connected');
-  socket.emit('connection', null);
-  socket.on('channel-join', id => {
-    console.log('channel join', id);
-    STATIC_CHANNELS.forEach(c => {
-      if (c.id === id) {
-        if (c.sockets.indexOf(socket.id) == (-1)) {
-          c.sockets.push(socket.id);
-          c.participants++;
-          io.emit('channel', c);
-        }
-      } else {
-        let index = c.sockets.indexOf(socket.id);
-        if (index != (-1)) {
-          c.sockets.splice(index, 1);
-          c.participants--;
-          io.emit('channel', c);
-        }
-      }
-    });
-
-    return id;
-  });
-
-  socket.on('send-message', message => {
-    io.emit('message', message);
-  });
-
-  socket.on('disconnect', () => {
-    STATIC_CHANNELS.forEach(c => {
-      let index = c.sockets.indexOf(socket.id);
-      if (index != (-1)) {
-        c.sockets.splice(index, 1);
-        c.participants--;
-        io.emit('channel', c);
-      }
-    });
-  });
-
-});
-
-app.get('/api/getChannels', (req, res) => {
-  res.json({
-    channels: STATIC_CHANNELS
-  })
-});
 
 // API
 app.post("/api/authenticate", loginController.postJWT);
