453571d198d10df7a58eb86de2f030815827e635
André Alcalde <andrealcalde2998@gmail.com>
2022-03-28 23:52:51 +0200

    console logs revisados 2.0 + componente mensajes terminado

--- routes/messages.js
+++ routes/messages.js
@@ -5,7 +5,7 @@ const Chats = require("../models/Chats");
 router.get("/:vendor", async (req, res, next) => {
   try {
     const vendedor = req.params.vendor;
-    const chats = await Chats.find({ vendedor: vendedor });
+    const chats = await Chats.find({ $or: [{ vendedor: vendedor }, { comprador: vendedor }] });
     res.json({ results: chats });
   } catch (err) {
     next(err);
